1a79872a44b00b27d3dd0c4d1f1b3412acfe02fb
Mohammed Benlamlih <45820673+benlamlih@users.noreply.github.com>
2025-06-19 21:52:28 +0200

    Initial Commit

---
+++ StoreRoute/StoreRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreRouteApp.swift
+//  StoreRoute
+//
+//  Created by Mohammed Benlamlih on 19/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct StoreRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoreRouteTests/StoreRouteTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoreRouteTests.swift
+//  StoreRouteTests
+//
+//  Created by Mohammed Benlamlih on 19/06/2025.
+//
+
+import Testing
+@testable import StoreRoute
+
+struct StoreRouteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
